9ce2e53d96618f4df23fcbcb5df4146673fd97e0
Nemanja Mijailovic <metalnem@mijailovic.net>
2019-02-07 08:30:23 +0100

    Allow fuzzing multiple BCL classes from the same project

--- CoreFX/CoreFX.Fuzz/Program.cs
+++ CoreFX/CoreFX.Fuzz/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
 using System.Xml;
 using SharpFuzz;
 
@@ -7,21 +10,48 @@ namespace CoreFX.Fuzz
 {
 	public class Program
 	{
+		private static readonly Dictionary<string, Action<string>> fuzzers =
+			new Dictionary<string, Action<string>>(StringComparer.OrdinalIgnoreCase)
+			{
+				{ "XmlReader.Create", XmlReader_Create },
+				{ "PEReader.GetMetadataReader", PEReader_GetMetadataReader }
+			};
+
 		public static void Main(string[] args)
 		{
-			Fuzzer.OutOfProcess.Run(() =>
+			var fuzzer = fuzzers[args[1]];
+			var path = args[0];
+
+			Fuzzer.OutOfProcess.Run(() => fuzzer(path));
+		}
+
+		private static void XmlReader_Create(string path)
+		{
+			try
 			{
-				try
+				using (var stream = File.OpenRead(path))
+				using (var xml = XmlReader.Create(stream))
 				{
-					using (var stream = File.OpenRead(args[0]))
-					using (var xml = XmlReader.Create(stream))
-					{
-						while (xml.Read()) { }
-					}
+					while (xml.Read()) { }
 				}
-				catch (IndexOutOfRangeException) { }
-				catch (XmlException) { }
-			});
+			}
+			catch (IndexOutOfRangeException) { }
+			catch (XmlException) { }
+		}
+
+		private static void PEReader_GetMetadataReader(string path)
+		{
+			try
+			{
+				using (var stream = File.OpenRead(path))
+				using (var pe = new PEReader(stream))
+				{
+					pe.GetMetadataReader();
+				}
+			}
+			catch (BadImageFormatException) { }
+			catch (InvalidOperationException) { }
+			catch (OverflowException) { }
 		}
 	}
 }
--- CoreFX/CoreFX.Run/Program.cs
+++ CoreFX/CoreFX.Run/Program.cs
@@ -1,16 +1,44 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
 using System.Xml;
 
 namespace CoreFX.Run
 {
 	public class Program
 	{
+		private static readonly Dictionary<string, Action<string>> runners =
+			new Dictionary<string, Action<string>>(StringComparer.OrdinalIgnoreCase)
+			{
+				{ "XmlReader.Create", XmlReader_Create },
+				{ "PEReader.GetMetadataReader", PEReader_GetMetadataReader }
+			};
+
 		public static void Main(string[] args)
 		{
-			using (var stream = File.OpenRead(args[0]))
+			var runner = runners[args[1]];
+			var path = args[0];
+
+			runner(path);
+		}
+
+		private static void XmlReader_Create(string path)
+		{
+			using (var stream = File.OpenRead(path))
 			using (var xml = XmlReader.Create(stream))
 			{
 				while (xml.Read()) { }
+			}
+		}
+
+		private static void PEReader_GetMetadataReader(string path)
+		{
+			using (var stream = File.OpenRead(path))
+			using (var pe = new PEReader(stream))
+			{
+				pe.GetMetadataReader();
 			}
 		}
 	}
