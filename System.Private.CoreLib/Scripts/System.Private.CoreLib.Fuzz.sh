4125c9508994b4313490fb77332a1f6b7ff5a7c0
Nemanja Mijailovic <metalnem@mijailovic.net>
2019-02-14 12:23:34 +0100

    Initial CoreLib fuzzing project

---
+++ System.Private.CoreLib/Scripts/System.Private.CoreLib.Fuzz.sh
@@ -0,0 +1,21 @@
+#/bin/sh
+set -eux
+
+cd ../System.Private.CoreLib.Fuzz
+mkdir -p ../Binaries
+mkdir -p ../Findings
+
+dotnet publish -r osx-x64 -o ../Binaries/"$1"
+cp ../OSX.x64.Debug/System.Private.CoreLib.dll ../Binaries/"$1"
+dotnet run --project /Users/Metalnem/Projects/sharpfuzz/src/SharpFuzz.CommandLine/SharpFuzz.CommandLine.csproj ../Binaries/"$1"/System.Private.CoreLib.dll
+
+cd ..
+rm -rf Findings/"$1"
+
+afl-fuzz \
+	-i Testcases/"$1" \
+	-o Findings/"$1" \
+	-t 5000 \
+	-m 10000 \
+	Binaries/"$1"/System.Private.CoreLib.Fuzz \
+	@@ "$1"
---
+++ System.Private.CoreLib/System.Private.CoreLib.Fuzz/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using SharpFuzz;
+
+namespace System.Private.CoreLib.Fuzz
+{
+	public class Program
+	{
+		private static readonly Dictionary<string, Action<string>> fuzzers =
+			new Dictionary<string, Action<string>>(StringComparer.OrdinalIgnoreCase)
+			{
+				{ "TimeSpan.TryParse", TimeSpan_TryParse },
+			};
+
+		public static void Main(string[] args)
+		{
+			var fuzzer = fuzzers[args[1]];
+			var path = args[0];
+
+			if (Environment.GetEnvironmentVariable("__AFL_SHM_ID") is null)
+			{
+				fuzzer(path);
+			}
+			else
+			{
+				Fuzzer.Run(() => fuzzer(path));
+			}
+		}
+
+		public static void TimeSpan_TryParse(string path)
+		{
+			var text = File.ReadAllText(path);
+			TimeSpan.TryParse(text, out _);
+		}
+	}
+}
