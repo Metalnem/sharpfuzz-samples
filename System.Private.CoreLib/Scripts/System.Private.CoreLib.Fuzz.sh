ac63e0d39a7f0e0dc289a9d8fff9049829e81d54
Nemanja Mijailovic <metalnem@mijailovic.net>
2019-02-15 11:21:23 +0100

    Add Linux and OSX scripts

--- System.Private.CoreLib/Scripts/System.Private.CoreLib.Fuzz.sh
+++ System.Private.CoreLib/Scripts/System.Private.CoreLib.Fuzz.sh
@@ -5,8 +5,8 @@ cd ../System.Private.CoreLib.Fuzz
 mkdir -p ../Binaries
 mkdir -p ../Findings
 
-dotnet publish -r osx-x64 -o ../Binaries/"$1"
-cp ../OSX.x64.Debug/System.Private.CoreLib.dll ../Binaries/"$1"
+dotnet publish -r "$2" -o ../Binaries/"$1"
+cp ../"$3"/System.Private.CoreLib.dll ../Binaries/"$1"
 sharpfuzz ../Binaries/"$1"/System.Private.CoreLib.dll
 
 cd ..
