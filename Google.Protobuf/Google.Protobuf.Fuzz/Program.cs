246d6d2b014ebc2025a78ed48068e2760d36b62b
Nemanja Mijailovic <metalnem@mijailovic.net>
2019-02-24 12:18:39 +0100

    Update protobuf fuzzer

--- Google.Protobuf/Google.Protobuf.Fuzz/Program.cs
+++ Google.Protobuf/Google.Protobuf.Fuzz/Program.cs
@@ -7,22 +7,33 @@ namespace Google.Protobuf.Fuzz
 {
 	public class Program
 	{
+		private static readonly MessageParser[] parsers = new MessageParser[]
+		{
+			M0.Parser, M1.Parser, M2.Parser, M3.Parser, M4.Parser,
+			M5.Parser, M6.Parser, M7.Parser, M8.Parser, M9.Parser,
+			M10.Parser, M11.Parser, M12.Parser, M13.Parser,
+			M14.Parser, M15.Parser, M16.Parser, M17.Parser,
+			M18.Parser, M19.Parser, M20.Parser, M21.Parser,
+			M22.Parser, M23.Parser, M24.Parser, M25.Parser
+		};
+
 		public static void Main(string[] args)
 		{
 			Fuzzer.Run(() =>
 			{
-				try
+				var bytes = File.OpenRead(args[0]);
+
+				foreach (var parser in parsers)
 				{
-					using (var file = File.OpenRead(args[0]))
+					try
 					{
-						Person.Parser.ParseFrom(file);
+						parser.ParseFrom(bytes);
 					}
+					catch (ArgumentException) { }
+					catch (InvalidOperationException) { }
+					catch (InvalidProtocolBufferException) { }
+					catch (OutOfMemoryException) { }
 				}
-				catch (ArgumentOutOfRangeException) { }
-				catch (ArgumentException) { }
-				catch (InvalidOperationException) { }
-				catch (InvalidProtocolBufferException) { }
-				catch (OutOfMemoryException) { }
 			});
 		}
 	}
--- Google.Protobuf/Google.Protobuf.Run/Program.cs
+++ Google.Protobuf/Google.Protobuf.Run/Program.cs
@@ -1,15 +1,35 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Sample;
 
 namespace Google.Protobuf.Run
 {
 	public class Program
 	{
+		private static readonly MessageParser[] parsers = new MessageParser[]
+		{
+			M0.Parser, M1.Parser, M2.Parser, M3.Parser, M4.Parser,
+			M5.Parser, M6.Parser, M7.Parser, M8.Parser, M9.Parser,
+			M10.Parser, M11.Parser, M12.Parser, M13.Parser,
+			M14.Parser, M15.Parser, M16.Parser, M17.Parser,
+			M18.Parser, M19.Parser, M20.Parser, M21.Parser,
+			M22.Parser, M23.Parser, M24.Parser, M25.Parser
+		};
+
 		public static void Main(string[] args)
 		{
-			using (var file = File.OpenRead(args[0]))
+			var bytes = File.OpenRead(args[0]);
+
+			foreach (var parser in parsers)
 			{
-				Person.Parser.ParseFrom(file);
+				try
+				{
+					parser.ParseFrom(bytes);
+				}
+				catch (ArgumentException) { }
+				catch (InvalidOperationException) { }
+				catch (InvalidProtocolBufferException) { }
+				catch (OutOfMemoryException) { }
 			}
 		}
 	}
