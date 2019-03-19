6e7ca40f348f79e860c525025f04a4ca2c48289b
Nemanja Mijailovic <metalnem@mijailovic.net>
2019-03-19 11:08:57 +0100

    Add CoreLib3 fuzzers for libFuzzer

--- System.Private.CoreLib3/System.Private.CoreLib.Fuzz/Program.cs
+++ System.Private.CoreLib3/System.Private.CoreLib.Fuzz/Program.cs
@@ -1,20 +1,33 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using SharpFuzz;
 
 namespace System.Private.CoreLib.Fuzz
 {
 	public class Program
 	{
-		private static readonly Dictionary<string, Action<string>> fuzzers =
+		private static readonly Dictionary<string, Action<string>> aflFuzz =
 			new Dictionary<string, Action<string>>(StringComparer.OrdinalIgnoreCase)
 			{
-				{ "Double.TryParse", Double_TryParse },
+				{ "Double.TryParse", Double_TryParse }
+			};
+
+		private static readonly Dictionary<string, ReadOnlySpanAction> libFuzzer =
+		new Dictionary<string, ReadOnlySpanAction>(StringComparer.OrdinalIgnoreCase)
+			{
+				{ "Double.TryParse", Double_TryParse }
 			};
 
 		public static void Main(string[] args)
 		{
-			var fuzzer = fuzzers[args[1]];
+			if (!(Environment.GetEnvironmentVariable("__LIBFUZZER_SHM_ID") is null))
+			{
+				Fuzzer.LibFuzzer.Run(libFuzzer[args[0]]);
+				return;
+			}
+
+			var fuzzer = aflFuzz[args[1]];
 			var path = args[0];
 
 			if (Environment.GetEnvironmentVariable("__AFL_SHM_ID") is null)
@@ -30,6 +43,22 @@ namespace System.Private.CoreLib.Fuzz
 		private static void Double_TryParse(string path)
 		{
 			var text = File.ReadAllText(path);
+
+			if (Double.TryParse(text, out var d1))
+			{
+				var s = d1.ToString("R");
+				var d2 = Double.Parse(s);
+
+				if (d1 != d2)
+				{
+					throw new Exception();
+				}
+			}
+		}
+
+		private static void Double_TryParse(ReadOnlySpan<byte> data)
+		{
+			var text = Encoding.UTF8.GetString(data);
 
 			if (Double.TryParse(text, out var d1))
 			{
