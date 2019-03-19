98277721aeb6e72737c3607ebb89918adf2ccb8f
Nemanja Mijailovic <metalnem@mijailovic.net>
2019-03-19 11:04:08 +0100

    Add initial CoreLib fuzzers for libFuzzer

--- System.Private.CoreLib2/System.Private.CoreLib.Fuzz/Program.cs
+++ System.Private.CoreLib2/System.Private.CoreLib.Fuzz/Program.cs
@@ -8,7 +8,7 @@ namespace System.Private.CoreLib.Fuzz
 {
 	public class Program
 	{
-		private static readonly Dictionary<string, Action<string>> fuzzers =
+		private static readonly Dictionary<string, Action<string>> aflFuzz =
 			new Dictionary<string, Action<string>>(StringComparer.OrdinalIgnoreCase)
 			{
 				{ "Convert.ToInt32", Convert_ToInt32 },
@@ -22,9 +22,22 @@ namespace System.Private.CoreLib.Fuzz
 				{ "UTF8Encoding.GetString", UTF8Encoding_GetString }
 			};
 
+		private static readonly Dictionary<string, ReadOnlySpanAction> libFuzzer =
+			new Dictionary<string, ReadOnlySpanAction>(StringComparer.OrdinalIgnoreCase)
+			{
+				{ "DateTime.TryParse", DateTime_TryParse },
+				{ "Double.TryParse", Double_TryParse }
+			};
+
 		public static void Main(string[] args)
 		{
-			var fuzzer = fuzzers[args[1]];
+			if (!(Environment.GetEnvironmentVariable("__LIBFUZZER_SHM_ID") is null))
+			{
+				Fuzzer.LibFuzzer.Run(libFuzzer[args[0]]);
+				return;
+			}
+
+			var fuzzer = aflFuzz[args[1]];
 			var path = args[0];
 
 			if (Environment.GetEnvironmentVariable("__AFL_SHM_ID") is null)
@@ -83,6 +96,22 @@ namespace System.Private.CoreLib.Fuzz
 			}
 		}
 
+		private static void DateTime_TryParse(ReadOnlySpan<byte> data)
+		{
+			var text = Encoding.UTF8.GetString(data);
+
+			if (DateTime.TryParse(text, out var dt1))
+			{
+				var s = dt1.ToString("O");
+				var dt2 = DateTime.Parse(s, null, DateTimeStyles.RoundtripKind);
+
+				if (dt1 != dt2)
+				{
+					throw new Exception();
+				}
+			}
+		}
+
 		private static void Decimal_Multiply(string path)
 		{
 			var text = File.ReadAllText(path);
@@ -105,6 +134,22 @@ namespace System.Private.CoreLib.Fuzz
 		private static void Double_TryParse(string path)
 		{
 			var text = File.ReadAllText(path);
+
+			if (Double.TryParse(text, out var d1))
+			{
+				var s = d1.ToString("G17");
+				var d2 = Double.Parse(s);
+
+				if (d1 != d2)
+				{
+					throw new Exception();
+				}
+			}
+		}
+
+		private static void Double_TryParse(ReadOnlySpan<byte> data)
+		{
+			var text = Encoding.UTF8.GetString(data);
 
 			if (Double.TryParse(text, out var d1))
 			{
