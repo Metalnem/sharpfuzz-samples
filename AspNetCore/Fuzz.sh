a6118d434acd4306462d9ee0d425a2bfc856a202
Nemanja Mijailovic <metalnem@mijailovic.net>
2019-04-11 13:14:44 +0200

    Fix Fuzz.sh

--- AspNetCore/Fuzz.sh
+++ AspNetCore/Fuzz.sh
@@ -4,7 +4,7 @@ set -eux
 rm -rf Binaries
 rm -rf Findings
 
-dotnet publish -r osx-x64 -o Binaries
+dotnet publish -r "$1" -o Binaries
 sharpfuzz Binaries/Microsoft.AspNetCore.Server.Kestrel.Core.dll
 
-afl-fuzz -i Testcases -o Findings -t 5000 -x Http.dict Binaries/AspNetCore.Fuzz
+afl-fuzz -i Testcases -o Findings -t 5000 -m 1000 -x Http.dict Binaries/AspNetCore.Fuzz
