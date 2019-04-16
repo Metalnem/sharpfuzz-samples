4d9bb93fa42269d32d32bc7583bb6676840173d2
Nemanja Mijailovic <metalnem@mijailovic.net>
2019-04-16 13:14:22 +0200

    Update the AspNetCore fuzzing script to download the latest binary version

--- AspNetCore/Fuzz.sh
+++ AspNetCore/Fuzz.sh
@@ -7,6 +7,9 @@ rm -rf Findings
 dotnet publish -r "$1" -o Binaries
 sharpfuzz Binaries/Microsoft.AspNetCore.Server.Kestrel.Core.dll
 
+wget -q https://github.com/Metalnem/sharpfuzz/releases/download/libfuzzer-proto-dotnet-0.0.1/libfuzzer-proto-dotnet-0.0.1.zip
+unzip -o libfuzzer-proto-dotnet-0.0.1.zip
+
 ./libfuzzer-proto-dotnet \
 	-timeout=5 \
 	-dict=Http.dict \
