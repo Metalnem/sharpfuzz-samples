30d3d8d09fd0da2d6d237ed5d548dbfe409b40dc
Nemanja Mijailovic <metalnem@mijailovic.net>
2019-07-02 14:15:10 +0200

    Switch to Docker for libFuzzer fuzzing

--- CoreFX/LibFuzzer/HttpUtility.UrlEncode.sh
+++
@@ -1,4 +0,0 @@
-#/bin/sh
-set -eux
-
-./CoreFX.Fuzz.sh HttpUtility.UrlEncode System.Web.HttpUtility.dll linux-x64
--- CoreFX/LibFuzzer/XmlReader.Create.sh
+++
@@ -1,4 +0,0 @@
-#/bin/sh
-set -eux
-
-./CoreFX.FuzzDict.sh XmlReader.Create System.Private.Xml.dll /usr/local/share/afl/dictionaries/xml.dict linux-x64
