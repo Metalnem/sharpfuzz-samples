7b61dc52ba2a2703dc32c098b42b70027788a0a8
Nemanja Mijailovic <metalnem@mijailovic.net>
2023-06-07 14:06:59 +0200

    Modernize Newtonsoft.Json

---
+++ Newtonsoft.Json/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SharpFuzz;
+
+namespace Newtonsoft.Json.Fuzz
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fuzzer.OutOfProcess.Run(s =>
+			{
+				try
+				{
+					JsonConvert.DeserializeObject(s);
+				}
+				catch { }
+			});
+		}
+	}
+}
