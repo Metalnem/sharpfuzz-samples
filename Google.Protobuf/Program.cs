cda7130a0f96d2de9a3b88bbcacb0fe13028e509
Nemanja Mijailovic <metalnem@mijailovic.net>
2023-06-07 22:13:23 +0200

    Modernize Google.Protobuf

--- Google.Protobuf/Program.cs
+++ Google.Protobuf/Program.cs
@@ -9,12 +9,32 @@ namespace Google.Protobuf.Fuzz
 	{
 		private static readonly MessageParser[] parsers = new MessageParser[]
 		{
-			M0.Parser, M1.Parser, M2.Parser, M3.Parser, M4.Parser,
-			M5.Parser, M6.Parser, M7.Parser, M8.Parser, M9.Parser,
-			M10.Parser, M11.Parser, M12.Parser, M13.Parser,
-			M14.Parser, M15.Parser, M16.Parser, M17.Parser,
-			M18.Parser, M19.Parser, M20.Parser, M21.Parser,
-			M22.Parser, M23.Parser, M24.Parser, M25.Parser
+			M0.Parser,
+			M1.Parser,
+			M2.Parser,
+			M3.Parser,
+			M4.Parser,
+			M5.Parser,
+			M6.Parser,
+			M7.Parser,
+			M8.Parser,
+			M9.Parser,
+			M10.Parser,
+			M11.Parser,
+			M12.Parser,
+			M13.Parser,
+			M14.Parser,
+			M15.Parser,
+			M16.Parser,
+			M17.Parser,
+			M18.Parser,
+			M19.Parser,
+			M20.Parser,
+			M21.Parser,
+			M22.Parser,
+			M23.Parser,
+			M24.Parser,
+			M25.Parser
 		};
 
 		public static void Main(string[] args)
@@ -32,11 +52,7 @@ namespace Google.Protobuf.Fuzz
 							memory.Seek(0, SeekOrigin.Begin);
 							parser.ParseFrom(memory);
 						}
-						catch (ArgumentException) { }
-						catch (IndexOutOfRangeException) { }
-						catch (InvalidOperationException) { }
-						catch (InvalidProtocolBufferException) { }
-						catch (OutOfMemoryException) { }
+						catch { }
 					}
 				}
 			});
